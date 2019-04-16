ce4bd43946344fd76e9acad0b621084957e1f35e
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-04-16 19:16:15 -0300

    temp

--- app/ui/client/lib/textarea-autogrow.js
+++ app/ui/client/lib/textarea-autogrow.js
@@ -1,101 +1,105 @@
 import _ from 'underscore';
+import { keyCodes } from '../../../ui-utils/client';
 
-const replaceWhitespaces = (whitespaces) => `${ '&nbsp;'.repeat(whitespaces.length - 1) } `;
+const createShadow = (textarea: HTMLTextAreaElement) => {
+	const shadow = document.createElement('div');
+	shadow.classList.add('autogrow-shadow');
+	document.body.appendChild(shadow);
 
-const getShadow = () => {
-	let shadow = document.getElementById('autogrow-shadow');
+	const width = textarea.clientWidth;
+	const { font, lineHeight } = window.getComputedStyle(textarea);
 
-	if (!shadow) {
-		shadow = document.createElement('div');
-		shadow.setAttribute('id', 'autogrow-shadow');
-		document.body.appendChild(shadow);
-	}
+	shadow.style.position = 'fixed';
+	shadow.style.top = '50%';
+	shadow.style.right = '0';
+	shadow.style.zIndex = '9999999';
+	shadow.style.backgroundColor = 'rgba(255, 127, 0, 0.5)';
+	// shadow.style.position = 'absolute';
+	// shadow.style.top = '-10000px';
+	// shadow.style.left = '-10000px';
+	shadow.style.width = `${ width }px`;
+	shadow.style.font = font;
+	shadow.style.lineHeight = lineHeight;
+	shadow.style.resize = 'none';
+	shadow.style.wordWrap = 'break-word';
 
 	return shadow;
 };
 
-$.fn.autogrow = function({ postGrowCallback } = {}) {
-	const shadow = getShadow();
+const replaceWhitespaces = (whitespaces: string) => `${ '&nbsp;'.repeat(whitespaces.length - 1) } `;
 
-	return this.filter('textarea').each((i, textarea) => {
-		const $textarea = $(textarea);
+const setupAutogrow = (textarea: HTMLTextAreaElement) => {
+	const shadow = createShadow(textarea);
+	const $textarea = $(textarea);
 
-		const trigger = _.debounce(() => $textarea.trigger('autogrow', []), 500);
+	const trigger = _.debounce(() => $textarea.trigger('autogrow', []), 500);
 
-		const width = $textarea.width();
-		const minHeight = $textarea.height();
-		const maxHeight = window.getComputedStyle(textarea)['max-height'].replace('px', '');
+	const width = textarea.clientWidth;
+	const height = textarea.clientHeight;
 
-		let lastWidth = width;
-		let lastHeight = minHeight;
-		let length = 0;
+	const minHeight = height;
+	const maxHeight = parseInt(window.getComputedStyle(textarea).maxHeight, 10);
 
-		shadow.style.position = 'absolute';
-		shadow.style.top = '-10000px';
-		shadow.style.left = '-10000px';
-		shadow.style.width = `${ width }px`;
-		shadow.style.fontSize = $textarea.css('fontSize');
-		shadow.style.fontFamily = $textarea.css('fontFamily');
-		shadow.style.fontWeight = $textarea.css('fontWeight');
-		shadow.style.lineHeight = `${ $textarea.css('lineHeight') }px`;
-		shadow.style.resize = 'none';
-		shadow.style.wordWrap = 'break-word';
+	let lastWidth = width;
+	let lastHeight = minHeight;
+	let textLenght = 0;
 
-		const update = (event) => {
-			const width = $textarea.width();
-			if (lastHeight >= maxHeight && length && length < textarea.value.length && width === lastWidth) {
-				return true;
-			}
+	const update = () => {
+		const { clientWidth: width, value: text } = textarea;
 
-			let val = textarea.value.replace(/</g, '&lt;')
-				.replace(/>/g, '&gt;')
-				.replace(/&/g, '&amp;')
-				.replace(/\n$/, '<br/>&nbsp;')
-				.replace(/\n/g, '<br/>')
-				.replace(/ {2,}/g, replaceWhitespaces);
+		const isMaximumHeightReached = lastHeight >= maxHeight;
+		const isTextLengthOutdated = textLenght && textLenght < text.length;
+		const wasWidthChanged = width !== lastWidth;
 
-			// Did enter get pressed?  Resize in this keydown event so that the flicker doesn't occur.
-			if (event && event.data && event.data.event === 'keydown' && event.keyCode === 13 && (event.shiftKey || event.ctrlKey || event.altKey)) {
-				val += '<br/>';
-			}
+		if (isMaximumHeightReached && isTextLengthOutdated && !wasWidthChanged) {
+			return true;
+		}
 
-			if (width !== lastWidth) {
-				shadow.style.width = `${ width }px`;
-				lastWidth = width;
-			}
+		const shadowText = text.replace(/</g, '&lt;')
+			.replace(/>/g, '&gt;')
+			.replace(/&/g, '&amp;')
+			.replace(/\n$/, '<br/>&nbsp;')
+			.replace(/\n/g, '<br/>')
+			.replace(/ {2,}/g, replaceWhitespaces);
 
-			shadow.innerHTML = val;
+		if (wasWidthChanged) {
+			shadow.style.width = `${ width }px`;
+			lastWidth = width;
+		}
 
-			let newHeight = Math.max(shadow.clientHeight + 1, minHeight) + 1;
+		shadow.innerHTML = shadowText;
 
-			let overflow = 'hidden';
+		let height = Math.max(shadow.clientHeight + 1, minHeight) + 1;
 
-			if (newHeight >= maxHeight) {
-				newHeight = maxHeight;
-				overflow = '';
-			} else {
-				length = textarea.value.length;
-			}
+		let overflow = 'hidden';
 
-			if (newHeight === lastHeight) {
-				return true;
-			}
+		if (height >= maxHeight) {
+			height = maxHeight;
+			overflow = '';
+		} else {
+			textLenght = text.length;
+		}
 
-			lastHeight = newHeight;
+		if (height === lastHeight) {
+			return true;
+		}
 
-			$textarea.css({ overflow, height: newHeight });
+		lastHeight = height;
 
-			trigger();
+		$textarea.css({ overflow, height });
 
-			postGrowCallback && postGrowCallback($textarea);
-		};
+		trigger();
+	};
 
-		const updateThrottled = _.throttle(update, 300);
+	const updateThrottled = _.throttle(update, 300);
 
-		$textarea.on('change input', updateThrottled);
-		$textarea.on('focus', update);
-		$(window).resize(updateThrottled);
-		update();
-		textarea.updateAutogrow = update;
-	});
+	$textarea.on('change input', updateThrottled);
+	$textarea.on('focus', update);
+	$(window).resize(updateThrottled);
+	update();
+	textarea.updateAutogrow = update;
+};
+
+$.fn.autogrow = function() {
+	return this.filter('textarea').each((i, textarea: HTMLTextAreaElement) => setupAutogrow(textarea));
 };
