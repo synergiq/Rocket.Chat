b2ec728c686c843d840f7baa1fe82e05c0002101
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2019-04-23 17:28:16 -0300

    [BREAK] Require OPLOG/REPLICASET to run Rocket.Chat

--- packages/rocketchat-mongo-config/server/index.js
+++ packages/rocketchat-mongo-config/server/index.js
@@ -1,8 +1,19 @@
-import { Mongo } from 'meteor/mongo';
+import { Mongo, MongoInternals } from 'meteor/mongo';
 
 const mongoOptionStr = process.env.MONGO_OPTIONS;
 if (typeof mongoOptionStr !== 'undefined') {
 	const mongoOptions = JSON.parse(mongoOptionStr);
 
 	Mongo.setConnectionOptions(mongoOptions);
+}
+
+const isOplogEnabled = MongoInternals.defaultRemoteCollectionDriver().mongo._oplogHandle && !!MongoInternals.defaultRemoteCollectionDriver().mongo._oplogHandle.onOplogEntry;
+
+if (!isOplogEnabled) {
+	console.error(`
+	OPLOG / REPLICASET is required to run Rocket.Chat.
+	More information at https://rocket.chat/docs/installation/docker-containers/high-availability-install/#install-mongodb-replicaset
+	`);
+
+	process.exit(1);
 }
